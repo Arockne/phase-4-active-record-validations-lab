24952d5955d6e972e52659c7a0c4cfde77ab9de1
Daniel Calvin <arockne2003@yahoo.com>
2022-03-07 10:31:42 -0700

    commit tree ab53ad3e2d1b27fad1f51a5e453d1f330329f3cc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait
+  
+
+  def click_bait
+    if title.present?
+      bait = %W(Won't\ Believe Secret Top Guess)
+      includes_bait =  bait.any? { |word| title.include?(word)}
+      if !includes_bait
+        errors.add(:title, "Title needs clickbait")
+      end
+    end
+  end
 end
